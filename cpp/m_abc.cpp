5648e6a93f3a33efff353d47fa1a546937d0753c
BuJ1 <julianbutscher@gmx.de>
2024-03-01 15:57:00 +0100

    [m_abc] Validated with different parameters

--- cpp/m_abc.cpp
+++ cpp/m_abc.cpp
@@ -64,20 +64,19 @@ int main()
 {
     // Define test values
     std::complex<double> k0(0.015707963267948967, 0.0);
-    std::complex<double> kx(-0.002427175521633375, 0.0);
+    std::complex<double> kx(-0.00002741555386204, 0.0);
     std::complex<double> ky(0.0, 0.0);
 
-    Eigen::Vector4cd v_kz{std::complex<double>(-0.01551931, 0.0), std::complex<double>(-0.01551931, 0.0), std::complex<double>(0.01551931, 0.0), std::complex<double>(0.01551931, 0.0)};
+    Eigen::Vector4cd v_kz{std::complex<double>(-0.01570794, 0.0), std::complex<double>(-0.01570794, 0.0), std::complex<double>(0.01570794, 0.0), std::complex<double>(0.01570794, 0.0)};
 
-    Eigen::Matrix4cd m_e;
-    m_e << std::complex<double>(0.98798985, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(-0.15451879, 0.0), std::complex<double>(0.0, 0.0),
-        std::complex<double>(0.0, 0.0), std::complex<double>(1.0, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(0.0, 0.0),
-        std::complex<double>(-0.98798985, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(-0.15451879, 0.0), std::complex<double>(0.0, 0.0),
-        std::complex<double>(0.0, 0.0), std::complex<double>(1.0, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(0.0, 0.0);
+    Eigen::Matrix4cd v_e{{std::complex<double>(0.99999848, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(-0.00174533, 0.0), std::complex<double>(0.0, 0.0)},
+                         {std::complex<double>(0.0, 0.0), std::complex<double>(1.0, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(0.0, 0.0)},
+                         {std::complex<double>(-0.99999848, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(-0.00174533, 0.0), std::complex<double>(0.0, 0.0)},
+                         {std::complex<double>(0.0, 0.0), std::complex<double>(1.0, 0.0), std::complex<double>(0.0, 0.0), std::complex<double>(0.0, 0.0)}};
 
     double d = 0.0;
 
-    auto [v_a, v_b, m_a12, m_a34, m_b12, m_b34, m_c12, m_c34] = m_abc(k0, kx, ky, v_kz, m_e, d);
+    auto [v_a, v_b, m_a12, m_a34, m_b12, m_b34, m_c12, m_c34] = m_abc(k0, kx, ky, v_kz, v_e, d);
 
     // Print the results
     std::cout << "v_a: " << std::endl
@@ -103,10 +102,10 @@ int main()
     // (-0,-0)
     //   (0,0)
     // v_b:
-    // (-0.156397,0)
-    //         (0,0)
-    // (0.156397,-0)
-    //         (0,0)
+    // (-0.00174533,0)
+    //   (0,0)
+    // (0.00174533,-0)
+    //   (0,0)
     // m_a12:
     // (1,0) (0,0)
     // (0,0) (1,0)
@@ -114,11 +113,11 @@ int main()
     //   (1,0)   (0,0)
     // (-0,-0)   (1,0)
     // m_b12:
-    //          (0,0)  (0.0155193,0)
-    // (-0.0158989,0)          (0,0)
+    // (0,0) (0.0157079,0)
+    // (-0.015708,0)         (0,0)
     // m_b34:
-    //          (0,0) (-0.0155193,0)
-    //  (0.0158989,0)          (0,0)
+    //  (0,0) (-0.0157079,0)
+    //   (0.015708,0)          (0,0)
     // m_c12:
     // (1,-0)  (0,0)
     //  (0,0) (1,-0)
