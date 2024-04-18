89f148d04be0bf1489d69d70bb5a285c622de5c6
rouxfl <florianleroux35@gmail.com>
2024-04-18 14:46:48 +0200

    New main to simplify running from command line

--- src/main.py
+++ src/main.py
@@ -1,17 +1,38 @@
+import argparse
 from FilterStack import FilterStack
 
-# Input file
-filter_definition_json_file_path = "../examples/AlPcCl_bandpass_840nm.json"
+## typical call: python main.py ../examples/AlPcCl_bandpass_840nm.json dual_annealing
+## default values are current_structure.json from last optimisation and dual_annealing
+
+# Create the parser
+parser = argparse.ArgumentParser()
+
+# Add the arguments
+parser.add_argument(
+    "--file",
+    type=str,
+    help="The input file path",
+    default="./temp/current_structure.json",
+)
+parser.add_argument(
+    "--optimisation_method",
+    type=str,
+    help="The optimisation method",
+    default="dual_annealing",
+)
+
+# Parse the arguments
+args = parser.parse_args()
 
 # Create filter stack
-filter_stack = FilterStack(filter_definition_json_file_path)
+filter_stack = FilterStack(args.file)
 
 # Optimize stack
 features = filter_stack.perform_optimisation(
-    "dual_annealing", save_optimized_to_file=True
+    args.optimisation_method, save_optimized_to_file=True
 )
 
-# Calculate the data for 
+# Calculate the data for
 filter_stack.calculate_ar_data(
     save_figure=True,
     save_data=True,
