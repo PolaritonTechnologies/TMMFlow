35d66fc6a3505e7547d0a201aabf69e171a8dbf5
rouxfl <florianleroux35@gmail.com>
2024-09-05 11:41:43 +0200

    Return of the tests

--- src/tests.py
+++ src/tests.py
@@ -1,131 +1,106 @@
-# tests.py performs basic unit testing for the optimisation module
-# command is pytest tests.py
-
 from FilterStack import FilterStack
-import pandas as pd
 import numpy as np
-import os
-
-#########################
-# Convert data exported from openFilters for comparison to our standard
-# dataframe format
+import pandas as pd
+import time
+import json
 
 
-def convert_open_filter_datastructure(file_path, columns):
-    """
-    Convert the weird open filter file format to something comparable to our
-    pandas dataframe
-    """
-    # Read the file line by line, skipping lines that start with whitespace
-    # followed by 'wavelength', 'Reflection', 'Transmission' or 'Absorption'
-    with open(file_path, "r") as f:
-        lines = [
-            line
-            for line in f
-            if not line.lstrip().startswith(
-                ("wavelength", "Reflection", "Transmission", "Absorption")
-            )
-        ]
+def execute_test():
 
-    # Convert the list of lines to a single string
-    data = "\n".join(lines)
+    # Updated tests.py 05/09/2024
+    json_path = "tests/TestStructure.json"
 
-    # Use StringIO to read the string as a file
-    from io import StringIO
+    # Read the JSON file
+    with open(json_path, "r") as file:
+        filter_json = json.load(file)
 
-    data_io = StringIO(data)
+    # Create filter stack
+    filter_stack = FilterStack(my_filter_dict=filter_json)
 
-    # Read the CSV data
-    df = pd.read_csv(data_io, sep="\s+", names=["wavelength", "R"])
+    # Wavelength is a numpy array of wavelengths in nm from 400 to 1200 nm spaced by 1 nm
+    # Polar angles is a numpy array of polar angles in degrees from 0 to 89 degrees spaced by 1 degree
+    # Azimuthal angles is a numpy array of azimuthal angles in degrees from 0 to 360 degrees spaced by 60 degree
+    # Target type is a string that is either "r" or "t"
+    # Polarization is a string that is either "s" or "p"
 
-    # Reorder the dataframe so that it contains
-    number_of_angles = int(np.size(columns) - 1)
-    unique_wavelength = np.unique(df.to_numpy().T[0])
-    intensity_data = (
-        df.to_numpy()
-        .T[1]
-        .reshape(int(number_of_angles), int(df.to_numpy().T[1].size / number_of_angles))
-    )
-    full_frame_data = np.concatenate(
-        [unique_wavelength.reshape(1, np.size(unique_wavelength)), intensity_data],
-        axis=0,
-    )
-    df_to_write = pd.DataFrame(full_frame_data.T, columns=columns)
+    wavelength = np.arange(400, 1200, 1)
+    polar_angles = np.arange(0, 90, 1)
+    # azimuthal_angles = np.arange(0, 360, 60)
+    azimuthal_angles = np.arange(0, 1, 1)
+    target_type = "r"
+    polarization = "s"
 
-    # Write dataframe to file
-    df_to_write.to_csv(
-        ".".join(np.append(file_path.split(".")[:-1], "_conv.csv")),
-        sep="\t",
-        index=False,
+    # Start a timer to evaluate how fast the calculation is executed using time library
+    print("Starting test timer...")
+    start = time.time()
+
+    # Calculate the AR data
+    filter_stack.calculate_ar_data(
+        wavelength=wavelength,
+        polar_angles=polar_angles,
+        azimuthal_angles=azimuthal_angles,
+        target_type=target_type,
+        polarization=polarization,
+        save_data=True,
+        save_to_test=True,
     )
 
+    # Stop the timer and output the duration inside the file test_performance.json where the first key is reference_test and the second is this_test
+    end = time.time()
+    duration = end - start
+    print("Duration: ", duration, " seconds")
 
-# convert_open_filter_datastructure("../tests/a_p_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
-# convert_open_filter_datastructure("../tests/a_s_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
-# convert_open_filter_datastructure("../tests/t_p_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
-# convert_open_filter_datastructure("../tests/t_s_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
-# convert_open_filter_datastructure("../tests/r_p_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
-# convert_open_filter_datastructure("../tests/r_s_flat_top.csv", ['wavelength', 0.0, 15.0, 30.0, 45.0, 60.0, 75.0, 89.0])
+    # Before updating performance dictionary, we need to know that the test is passed in order to do so we compare test_to_compare.csv to this_test.csv
+    # If this_test.csv does not exist then output that the reference test did not exist and return false
+    # If this_test.csv does exist, compare the two files
+    # If the two files are the same, output that the test passed and return true
+    # If the two files are different, output that the test failed and return false
 
-#########################
-# Input parameters
-# json_file_path = os.path.join(
-    # os.path.dirname(os.getcwd()), "examples", "demo_test_with_backside.json"
-# )
-json_file_path = os.path.join(
-    os.path.dirname(os.getcwd()), "examples", "demo_test.json"
-)
+    # Check if this_test.csv exists
+    try:
+        with open("tests/this_test.csv", "r") as file:
+            # load it with pandas
+            this_test = pd.read_csv(file)
+    except FileNotFoundError:
+        print("Reference test does not exist, created with this execution")
+        return False
 
-#########################
+    # Check if this_test.csv is the same as test_to_compare.csv
+    with open("tests/test_reference.csv", "r") as file:
+        test_to_compare = pd.read_csv(file)
 
-# Create filter stack
-filter_stack = FilterStack(json_file_path)
+    # Check if the two dataframes are the same with a certain threshold
 
-# def test():
-# assert optimization.check_targets(cavity_optimisation())
-for target in ["t", "r"]:
-    for polarization in ["s", "p"]:
-        print("Target: " + target + ", Pol.: " + polarization)
-        filter_stack.calculate_ar_data(
-            polar_angles=[0, 15, 30, 45, 60, 75, 89],
-            save_data=True,
-            target_type=target,
-            polarization=polarization,
-        )
-        calculated_data_df = pd.read_csv(
-            os.path.join(os.getcwd(), "temp", "value.csv"), sep=","
-        )
-        calculated_data_df = calculated_data_df.rename(
-            columns={"Unnamed: 0": "wavelength"}
-        )
+    # Assuming this_test and test_to_compare are your DataFrames
+    # Calculate the percentage of matching elements
+    similarity_threshold = 0.995
+    matching_elements = (this_test == test_to_compare).sum().sum()
+    total_elements = this_test.size
+    similarity_percentage = matching_elements / total_elements
 
-        # open_filter_df = pd.read_csv(
-        #     "../tests/" + polarization + "_" + target + ".csv", sep="\t"
-        # )
-        open_filter_df = pd.read_csv(
-            os.path.join(os.path.dirname(os.getcwd()), "tests")
-            + "/"
-            + polarization
-            + "_"
-            + target
-            + "_with_backside.csv",
-            sep="\t",
-        )
+    if similarity_percentage < similarity_threshold:
+        return False
 
-        # complete_ease_df = pd.read_csv("../tests/" + polarization + "_" + target + "_CE.csv", sep = "\t")
+    # If the test passed, update the performance dictionary
+    # If test_performance.json does not exist, create it and set the reference_test to the duration of this test
+    try:
+        with open("tests/test_performance.json", "r") as file:
+            performance_dict = json.load(file)
+    except FileNotFoundError:
+        performance_dict = {"reference_test": duration}
+    # If test_performance.json exists, set the this_test key to the duration of this test
+    performance_dict["this_test"] = duration
 
-        # x = 1e-3
-        # calculated_data_df = calculated_data_df.where(calculated_data_df >= x, 0)
-        # open_filter_df = open_filter_df.where(open_filter_df >= x, 0)
-        temp_diff = abs(calculated_data_df - open_filter_df)
-        # temp_diff_ce_of = abs(complete_ease_df - open_filter_df)
-        # print(temp_diff)
-        # print(temp_diff_ce_of)
-        print("Maximum deviations: ")
-        print(temp_diff.max())
-        # print(temp_diff_ce_of.max())
+    # Write the updated performance dictionary to test_performance.json
+    with open("tests/test_performance.json", "w") as file:
+        json.dump(performance_dict, file)
+    return True
 
-        # Plot this to get a feeling for where the inaccuracies are the worst
 
-        # Testing with assert is not really helpful here
-        # pd.testing.assert_frame_equal(calculated_data_df, open_filter_df)
+if __name__ == "__main__":
+    test_result = execute_test()
+
+    if test_result:
+        print("Test passed")
+    else:
+        print("!!! Test failed !!!")
